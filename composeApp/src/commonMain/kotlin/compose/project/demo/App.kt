2bc8e85481f334fa8393b2fa0b9c9661f5848551
masaibar <masaibar.dev@gmail.com>
2024-12-27 14:30:23 +0900

    プルダウンでタイムゾーンを選択できるようにリファクタ

--- composeApp/src/commonMain/kotlin/compose/project/demo/App.kt
+++ composeApp/src/commonMain/kotlin/compose/project/demo/App.kt
@@ -1,12 +1,14 @@
 package compose.project.demo
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,7 +21,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.datetime.Clock
-import kotlinx.datetime.IllegalTimeZoneException
 import kotlinx.datetime.LocalTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
@@ -27,9 +28,9 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
-fun App() {
+fun App(countries: List<Country> = countries()) {
     MaterialTheme {
-        var location by remember { mutableStateOf("Europe/Paris") }
+        var showCountries by remember { mutableStateOf(false) }
         var timeAtLocation by remember { mutableStateOf("No location selected") }
         Column(
             modifier = Modifier.padding(20.dp)
@@ -40,30 +41,53 @@ fun App() {
                 textAlign = TextAlign.Center,
                 modifier = Modifier.fillMaxWidth().align(Alignment.CenterHorizontally)
             )
-            TextField(
-                value = location,
-                modifier = Modifier.padding(top = 10.dp),
-                onValueChange = { location = it }
-            )
+            Row(modifier = Modifier.padding(start = 20.dp, top = 10.dp)) {
+                DropdownMenu(
+                    expanded = showCountries,
+                    onDismissRequest = { showCountries = false }
+                ) {
+                    countries.forEach { country ->
+                        DropdownMenuItem(
+                            onClick = {
+                                timeAtLocation = currentTimeAt(country)
+                                showCountries = false
+                            }
+                        ) {
+                            Text(country.name)
+                        }
+                    }
+                }
+            }
+
             Button(
                 modifier = Modifier.padding(top = 10.dp),
-                onClick = { timeAtLocation = currentTimeAt(location) ?: "Invalid location" }
+                onClick = {
+                    showCountries = !showCountries
+                }
             ) {
-                Text("Show Time At Location")
+                Text("Select Location")
             }
         }
     }
 }
 
-fun currentTimeAt(location: String): String? {
+data class Country(
+    val name: String,
+    val zone: TimeZone
+)
+
+fun countries(): List<Country> = listOf(
+    Country("Japan", TimeZone.of("Asia/Tokyo")),
+    Country("France", TimeZone.of("Europe/Paris")),
+    Country("Mexico", TimeZone.of("America/Mexico_City")),
+    Country("Indonesia", TimeZone.of("Asia/Jakarta")),
+    Country("Egypt", TimeZone.of("Africa/Cairo"))
+)
+
+fun currentTimeAt(country: Country): String {
     fun LocalTime.formatted() = "$hour:$minute:$second"
 
-    return try {
-        val time = Clock.System.now()
-        val zone = TimeZone.of(location)
-        val localTime = time.toLocalDateTime(zone).time
-        "The time in $location is ${localTime.formatted()}"
-    } catch (ex: IllegalTimeZoneException) {
-        null
-    }
+    val time = Clock.System.now()
+    val localTime = time.toLocalDateTime(country.zone).time
+    return "The time in ${country.name} is ${localTime.formatted()}"
 }

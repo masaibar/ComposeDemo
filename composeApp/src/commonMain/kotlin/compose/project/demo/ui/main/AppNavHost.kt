8a28a3f7e3efba68570f10e8d481abf91530843d
masaibar <masaibar.dev@gmail.com>
2024-12-29 16:53:16 +0900

    バージョン下げ

--- composeApp/src/commonMain/kotlin/compose/project/demo/ui/main/AppNavHost.kt
+++ composeApp/src/commonMain/kotlin/compose/project/demo/ui/main/AppNavHost.kt
@@ -1,12 +1,12 @@
 package compose.project.demo.ui.main
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun AppNavHost(navController: NavHostController = rememberNavController()) {
+fun AppNavHost() {
+    val navController = rememberNavController()
     NavHost(
         navController = navController,
         startDestination = mainScreenRoute
